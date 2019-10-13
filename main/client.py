c19f2bbff6bf87887d942fe29dbabca14a02f097
thebosak <33043855+thebosak@users.noreply.github.com>
2019-10-13 15:58:24 -0500

    modifying client, server, and adding new port to CA

--- main/client.py
+++ main/client.py
@@ -4,21 +4,46 @@ Created on October 9, 2019
 @author: Brad Bosak
 '''
 import socket
-def createClient():
-    #Create client socket
-    clientSocket = socket.socket()
+from Crypto.Cipher import Salsa20
+
+class Client:
+    def clientToServer(self):
+        #Create client socket
+        clientSocket = socket.socket()
+        
+        #Define port
+        serverPort = 9500
+        
+        #Connect to server to get server's name
+        clientSocket.connect(('127.0.0.1', serverPort))
+            
+        serverMessage = input('Enter what you want from the server here: ')
+        #Send message to server
+        clientSocket.send(serverMessage.encode())
+        #Receive and print server name
+        serverName = clientSocket.recv(1024).decode()
+        print("The server name is ", serverName)
+        return serverName
     
-    #Define port
-    port = 9500
+    def clientToCA(self, serverName):
+        #Create client socket
+        clientSocket = socket.socket()
+        
+        #Define port
+        caPort = 9501
+        #Connect to CA
+        clientSocket.connect(('127.0.0.1', caPort))
+        caMessage = serverName
+        #Send message to CA
+        clientSocket.send(caMessage.encode())
+        #Receive and print public key from CA
+        dataFromCertificateAuthority = clientSocket.recv(1024).decode()
+        print ("Message from CertificateAuthority is", dataFromCertificateAuthority)
+        
+def main():
+    client = Client()
+    serverName = client.clientToServer()
+    client.clientToCA(serverName)
     
-    #Connect socket
-    clientSocket.connect(('127.0.0.1', port))
-    clientMessage = input("Enter your message to send to the server: ")
-    #Send message to client
-    clientSocket.send(clientMessage.encode())
-    #Receive and print message from server
-    dataFromServer = clientSocket.recv(1024).decode()
-    print ("Message from server is", dataFromServer)
-    
-#Call main function
-createClient()
+ 
+main()
--- main/server.py
+++ main/server.py
@@ -4,34 +4,45 @@ Created on October 9, 2019
 @author: Brad Bosak
 '''
 import socket
-import sys  
+import sys
+# from certificateAuthority import CertificateAuthority
+# from client import Client
 
-def createServer():
-    #Create server socket
-    serverSocket = socket.socket()          
-    
-    #Define port and bind
-    port = 9500
-    serverSocket.bind(('', port))
-    
-    serverSocket.listen(5)      
-    print ("Server socket is created, binded to port {0}, and listening".format(port))
-    
-    while True:
+class Server:
+    def createServer(self):
+        #Create server socket
+        serverSocket = socket.socket()          
         
-        #Find client
-        (clientSocket, address) = serverSocket.accept()
-        print ("Connection found from ", address)
-        dataFromClient = clientSocket.recv(1024).decode()
+        #Define port and bind
+        caPort = 9501
+        port = 9500
         
-        #Logic around messages
-        if dataFromClient == "Hello":
-            returnMessage = "Hi"
-        else:
-            returnMessage = "Goodbye"
-        #Print client message and send return message
-        print ('Message from client is:', dataFromClient)
-        clientSocket.send(returnMessage.encode())
-
-#Call main function
-createServer()
+        serverSocket.bind(('', port))
+        serverSocket.listen(5)
+              
+        print ("Server socket is created, binded to port {0}, and listening".format(port))
+        
+        #server attributes
+        serverName="BradsServer"
+        
+        while True:
+            
+            #Find client
+            (clientSocket, address) = serverSocket.accept()
+            print ("Connection found from ", address)
+            dataFromClient = clientSocket.recv(1024).decode()
+            
+            #Logic around messages
+            if dataFromClient == "Name":
+                returnMessage = serverName
+            else:
+                returnMessage = "Goodbye"
+            #Print client message and send return message
+            print ('Message from client is:', dataFromClient)
+            clientSocket.send(returnMessage.encode())
+    
+def main():
+    server = Server()
+    server.createServer()
+ 
+main()
